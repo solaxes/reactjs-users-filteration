c22085fa31c1a6673f0a81c12db9c3d15e266c9d
Jaidev Bangar <jaidevbangar@192.168.1.3>
2022-11-26 09:22:21 +0530

    Small user filteration app

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import "./App.css";
+import React, { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  // fetch users and populate in monsters array when component is mounted
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <SearchBox
+          className="monster-search-box"
+          onChangeHandler={onSearchChange}
+          placeholder="Search monsters"
+        />
+
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,25 @@
+import { Component } from "react";
+import "./card-list.styles.css";
+
+class CardList extends Component {
+  render() {
+    console.log(this.props);
+    const { monsters } = this.props;
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => (
+          <div className="card-container" key={monster.id}>
+            <img
+              alt={`monster ${monster.name}`}
+              src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+            />
+            <h2>{monster.name}</h2>
+            <p>{monster.email}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CardList;
